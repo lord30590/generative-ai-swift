e6a4b6aa0ebda9f85de1aba4481cc4f244603571
浩龙 <ihaolong@icloud.com>
2023-12-19 10:56:37 +0800

    Add proxy host to gemini

--- Sources/GoogleAI/GenerativeAISwift.swift
+++ Sources/GoogleAI/GenerativeAISwift.swift
@@ -21,5 +21,6 @@ import Foundation
 public enum GenerativeAISwift {
   /// String value of the SDK version
   public static let version = "0.4.4"
-  static let baseURL = "https://generativelanguage.googleapis.com/v1"
+//  static let baseURL = "https://generativelanguage.googleapis.com/v1"
+    static let baseURL = "https://gemini.emoteai.club/v1"// proxy host
 }
